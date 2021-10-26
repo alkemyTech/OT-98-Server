e49417b2cd05eae8ce9e26af423924eff189c9e0
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-26 13:51:54 -0300

    Email sender interface and class added

---
+++ src/main/java/com/alkemy/ong/util/emailsender/EmailService.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.util.emailsender;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+@Service
+public class EmailService {
+
+  @Value("${email.sender.from}")
+  private String emailFrom;
+
+  @Value("${email.sender.sendgrid.token}")
+  private String sendGridToken;
+
+  public String sendEmail(IEmailSend emailBody) throws IOException {
+    Email from = new Email(emailFrom);
+    String subject = emailBody.getSubject();
+    Email to = new Email(emailBody.getEmailTo());
+    Content content = emailBody.getEmailContent();
+
+    Mail mail = new Mail(from, subject, to, content);
+    SendGrid sg = new SendGrid(sendGridToken);
+    Request request = new Request();
+
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint("mail/send");
+      request.setBody(mail.build());
+      Response response = sg.api(request);
+      return response.getBody();
+    } catch (IOException e) {
+      throw e;
+    }
+  }
+}
