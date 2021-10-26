b8c4ef498fae70c0b3885a9ad0a42bfb0258659c
ignaciomontovio <ignaciomontovio@gmail.com>
2021-10-26 12:00:31 -0300

    OT98-15: Fix organization style

--- src/main/java/com/alkemy/ong/model/entity/Organization.java
+++ src/main/java/com/alkemy/ong/model/entity/Organization.java
@@ -1,67 +1,60 @@
 package com.alkemy.ong.model.entity;
 
 import java.sql.Timestamp;
-import java.time.Instant;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="ORGANIZATIONS")
 public class Organization {
 
 	@Id
-	@Column(name="ID")
+	@Column(name="ORGANIZATIONS_ID")
+	@Setter(AccessLevel.NONE)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private long id;
+	
 	@Column(name="NAME",nullable = false)
 	private String name;
+	
 	@Column(name="IMAGE",nullable = false)
 	private String image;
+	
 	@Column(name="ADDRESS",nullable = true)
 	private String address;
+	
 	@Column(name="PHONE",nullable = true)
 	private Integer phone;
+	
 	@Column(name="EMAIL",nullable = false)
 	private String email;
+	
 	@Column(name="WELCOME_TEXT",nullable = false)
 	private String welcomeText;
+	
 	@Column(name="ABOUT_US_TEXT",nullable = true)
 	private String aboutUsText;
-	@Column(name="TIMESTAMP")
-	private Timestamp timeStamp;
-	@Column(name="SOFT_DELETE")
-	private Boolean softDelete;
 	
-	public Organization(String name, String image, String address, Integer phone, String email, String welcomeText,
-			String aboutUsText) {
-		super();
-		this.name = name;
-		this.image = image;
-		this.address = address;
-		this.phone = phone;
-		this.email = email;
-		this.welcomeText = welcomeText;
-		this.aboutUsText = aboutUsText;
-		timeStamp= Timestamp.from(Instant.now());
-	}
-
-
-
-	public Organization() {
-		super();
-		timeStamp= Timestamp.from(Instant.now());
-	}
+	@Column(name="TIMESTAMP")
+	@CreationTimestamp
+	private Timestamp timeStamp;
+	
+	@Column(name="SOFT_DELETE")
+	private boolean softDelete;
 	
 	
 }
