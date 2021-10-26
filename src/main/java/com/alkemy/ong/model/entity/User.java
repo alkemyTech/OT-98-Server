e467f0a6202c4cfce21453c68f1d4a28b2fb436a
matiasutn2019 <75179524+matiasutn2019@users.noreply.github.com>
2021-10-26 16:59:26 -0300

    OT98-17: create user entity (#3)

---
+++ src/main/java/com/alkemy/ong/model/entity/User.java
@@ -0,0 +1,63 @@
+package com.alkemy.ong.model.entity;
+
+import java.sql.Timestamp;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  @Column(name = "USERS_ID")
+  private Long id;
+
+  @Column(name = "FIRST_NAME", nullable = false)
+  private String firstName;
+
+  @Column(name = "LAST_NAME", nullable = false)
+  private String lastName;
+
+  @Column(name = "EMAIL", unique = true, nullable = false)
+  private String email;
+
+  @Column(name = "PASSWORD", nullable = false)
+  private String password;
+
+  @Column(name = "PHOTO", nullable = true)
+  private String photo;
+
+  @JoinColumn(name = "ROLES_ID")
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+  private List<Role> roles;
+
+  @CreationTimestamp
+  @Column(name = "TIMESTAMP", nullable = true)
+  private Timestamp timestamp;
+
+  @Column(name = "SOFT_DELETED", nullable = true)
+  private boolean softDeleted;
+}
+
+
