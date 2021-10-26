e4e6f15c669c64139efa443bd28ca584671b44d4
Magali Kain <mkain@mulesoft.com>
2021-10-26 18:14:53 -0300

    OT98-21: Apply code-style

--- src/main/java/com/alkemy/ong/model/entity/Activity.java
+++ src/main/java/com/alkemy/ong/model/entity/Activity.java
@@ -1,21 +1,18 @@
 package com.alkemy.ong.model.entity;
 
 import java.sql.Timestamp;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "ACTIVITIES")
@@ -25,25 +22,26 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Activity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Setter(AccessLevel.NONE)
-	@Column(name = "ACTIVITIES_ID")
-	private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  @Column(name = "ACTIVITIES_ID")
+  private long id;
 
-	@Column(name = "NAME", nullable = false)
-	private String name;
+  @Column(name = "NAME", nullable = false)
+  private String name;
 
-	@Column(name = "CONTENT", nullable = false)
-	private String content;
+  @Column(name = "CONTENT", nullable = false)
+  private String content;
 
-	@Column(name = "IMAGE", nullable = false)
-	private String image;
+  @Column(name = "IMAGE", nullable = false)
+  private String image;
 
-	@Column(name = "TIMESTAMPS")
-	@CreationTimestamp
-	private Timestamp timestamps;
+  @Column(name = "TIMESTAMPS")
+  @CreationTimestamp
+  private Timestamp timestamps;
 
-	@Column(name = "SOFT_DELETE")
-	private boolean softDelete;
+  @Column(name = "SOFT_DELETE")
+  private boolean softDelete;
+
 }
