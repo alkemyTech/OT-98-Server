16bab56dda0cc731fa16e18ee220cc8c5e14e2db
Magali Kain <mkain@mulesoft.com>
2021-10-26 13:46:51 -0300

    OT98-22: sort imports with the code-style

--- src/main/java/com/alkemy/ong/model/entity/Member.java
+++ src/main/java/com/alkemy/ong/model/entity/Member.java
@@ -1,18 +1,18 @@
 package com.alkemy.ong.model.entity;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
+import java.sql.Timestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.sql.Timestamp;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "MEMBERS")
