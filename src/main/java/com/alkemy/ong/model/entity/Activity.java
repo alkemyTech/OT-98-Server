997b4bac0b66f2aad6ac841f2d90ae5e6cd53c62
Joaquin Aman <Joaquin Aman>
2021-10-26 18:08:37 -0300

    Adding Activity.java

---
+++ src/main/java/com/alkemy/ong/model/entity/Activity.java
@@ -0,0 +1,49 @@
+package com.alkemy.ong.model.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ACTIVITIES")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter(AccessLevel.NONE)
+	@Column(name = "ACTIVITIES_ID")
+	private long id;
+
+	@Column(name = "NAME", nullable = false)
+	private String name;
+
+	@Column(name = "CONTENT", nullable = false)
+	private String content;
+
+	@Column(name = "IMAGE", nullable = false)
+	private String image;
+
+	@Column(name = "TIMESTAMPS")
+	@CreationTimestamp
+	private Timestamp timestamps;
+
+	@Column(name = "SOFT_DELETE")
+	private boolean softDelete;
+}
