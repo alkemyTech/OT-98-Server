8cd243b2cebcad84f77632c2046b8d98c5b11b2a
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-10-26 14:21:50 -0300

    several changes added

--- src/main/java/com/alkemy/ong/model/entity/User.java
+++ src/main/java/com/alkemy/ong/model/entity/User.java
@@ -1,70 +1,69 @@
 package com.alkemy.ong.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Filter;
-import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.ParamDef;
-import org.hibernate.annotations.SQLDelete;
 
-import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
+import lombok.Setter;
 
 @Entity
 @Table(name = "USERS")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@SQLDelete(sql = "UPDATE USERS SET DELETED = TRUE WHERE ID=?")
-@FilterDef(
-        name = "deletedUserFilter",
-        parameters = @ParamDef(name = "isDeleted", type = "boolean")
-)
-@Filter(
-        name = "deletedUserFilter",
-        condition = "deleted = :isDeleted"
-)
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  @Column(name = "ID")
+  private Long id;
 
-    @Column(name = "FIRST_NAME", nullable = false)
-    private String firstName;
+  @Column(name = "FIRST_NAME", nullable = false)
+  private String firstName;
 
-    @Column(name = "LAST_NAME", nullable = false)
-    private String lastName;
+  @Column(name = "LAST_NAME", nullable = false)
+  private String lastName;
 
-    @Column(name = "EMAIL", unique = true, nullable = false)
-    private String email;
+  @Column(name = "EMAIL", unique = true, nullable = false)
+  private String email;
 
-    @Column(name = "PASSWORD", nullable = false)
-    private String password;
+  @Column(name = "PASSWORD", nullable = false)
+  private String password;
 
-    @Column(name = "PHOTO", nullable = true)
-    private String photo;
+  @Column(name = "PHOTO", nullable = true)
+  private String photo;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "USER_ROLE",
-            joinColumns = {
-                    @JoinColumn(name = "USER_ID")
-            },
-            inverseJoinColumns = {
-                    @JoinColumn(name = "ROLE_ID")
-            }
-    )
-    private List<Role> roles;
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "USER_ROLE",
+      joinColumns = {
+          @JoinColumn(name = "USER_ID")
+      },
+      inverseJoinColumns = {
+          @JoinColumn(name = "ROLE_ID")
+      }
+  )
+  private List<Role> roles;
 
-    @Column(name = "TIMESTAMP", nullable = false)
-    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+  @Column(name = "TIMESTAMP", nullable = false)
+  private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-    @Column(name = "DELETED", nullable = false)
-    private boolean deleted = Boolean.FALSE;
+  @Column(name = "SOFT_DELETED", nullable = false)
+  private boolean softDeleted;
 }
 
 
