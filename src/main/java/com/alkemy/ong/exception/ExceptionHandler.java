6dcbf94304feb092b2ca3b28f56bbf42a3f49ff2
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-26 19:53:29 -0300

    Change Exception handler and Exception response for make them more reutilizable

--- src/main/java/com/alkemy/ong/exception/ExceptionHandler.java
+++ src/main/java/com/alkemy/ong/exception/ExceptionHandler.java
@@ -2,16 +2,17 @@ package com.alkemy.ong.exception;
 
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import com.alkemy.ong.model.response.ExceptionResponse;
+import com.alkemy.ong.model.response.ErrorResponse;
 
 @ControllerAdvice
 public class ExceptionHandler {
 
   @org.springframework.web.bind.annotation.ExceptionHandler(IOException.class)
-  public ResponseEntity<?> IOExceptions(HttpServletRequest request, Exception e) {
-    return ResponseEntity.badRequest().body(new ExceptionResponse(e, request.getRequestURI()));
+  public ResponseEntity<?> handleIOException(HttpServletRequest request, IOException e) {
+    return ResponseEntity.badRequest().body(new ErrorResponse(e, HttpStatus.BAD_REQUEST.value()));
   }
 
 }
