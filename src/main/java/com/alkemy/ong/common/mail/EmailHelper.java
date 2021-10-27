0c5faafc134fc282589e5a0a118d60456fdf6a70
Kevin Raimo Lugo <79285635+kevinraim@users.noreply.github.com>
2021-10-27 16:52:51 -0300

    OT98-27: email sender service added (#10)

---
+++ src/main/java/com/alkemy/ong/common/mail/EmailHelper.java
@@ -0,0 +1,51 @@
+package com.alkemy.ong.common.mail;
+
+import com.alkemy.ong.exception.SendEmailException;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailHelper {
+
+  @Value("${email.sender.from}")
+  private String emailFrom;
+
+  @Value("${email.sender.sendgrid.token}")
+  private String sendGridToken;
+
+  private static final String SEND_ENDPOINT = "mail/send";
+
+  public void send(IEmail emailBody) throws SendEmailException {
+    Email from = new Email(emailFrom);
+    String subject = emailBody.getSubject();
+    Email to = new Email(emailBody.getEmailTo());
+    Content content =
+        new Content(emailBody.getContent().getType(), emailBody.getContent().getValue());
+
+    Mail mail = new Mail(from, subject, to, content);
+    SendGrid sendGrid = new SendGrid(sendGridToken);
+    Request request = new Request();
+
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint(SEND_ENDPOINT);
+      request.setBody(mail.build());
+      Response response = sendGrid.api(request);
+
+      if (!(response.getStatusCode() >= 200 || response.getStatusCode() < 300)) {
+        throw new SendEmailException("The email has not sent");
+      }
+
+    } catch (IOException e) {
+      throw new SendEmailException(e.getMessage());
+    }
+  }
+}
---
+++ src/test/java/com/alkemy/ong/integration/SendEmailIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.alkemy.ong.integration;
+
+import com.alkemy.ong.common.mail.EmailHelper;
+import com.alkemy.ong.common.mail.IContent;
+import com.alkemy.ong.common.mail.IEmail;
+import com.alkemy.ong.exception.SendEmailException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+public class SendEmailIntegrationTest {
+
+  @Autowired
+  private EmailHelper emailHelper;
+
+  @Test
+  public void shouldSendEmail() throws SendEmailException {
+    emailHelper.send(new EmailTest());
+  }
+
+  public static class EmailTest implements IEmail, IContent {
+
+    @Override
+    public String getEmailTo() {
+      return "kevinraimolugo@gmail.com";
+    }
+
+    @Override
+    public String getSubject() {
+      return "This is a test";
+    }
+
+    @Override
+    public IContent getContent() {
+      return this;
+    }
+
+    @Override
+    public String getType() {
+      return "text/plain";
+    }
+
+    @Override
+    public String getValue() {
+      return "Hi, this is a test";
+    }
+
+  }
+}
