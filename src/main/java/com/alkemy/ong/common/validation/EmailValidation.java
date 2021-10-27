739c0b8c24beff150f5aba477b55f7c7d07e01eb
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-27 11:37:21 -0300

    OT98-25: Email validation changed text format

--- src/main/java/com/alkemy/ong/common/validation/EmailValidation.java
+++ src/main/java/com/alkemy/ong/common/validation/EmailValidation.java
@@ -5,14 +5,15 @@ import java.util.regex.Pattern;
 
 public class EmailValidation {
 
+  private static final String EMAIL_PATTERN =
+      "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+
   public static boolean isValid(String email) {
-
-    if (email == null)
+    if (email == null) {
       return false;
+    }
 
-    String ePattern =
-        "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-    Pattern p = java.util.regex.Pattern.compile(ePattern);
+    Pattern p = java.util.regex.Pattern.compile(EMAIL_PATTERN);
     Matcher m = p.matcher(email);
     return m.matches();
   }
