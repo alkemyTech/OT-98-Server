4a1b5292cc365b689a76087af8448020a38e1546
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-27 02:52:22 -0300

    BCryptPasswordEncoder bean added

--- src/main/java/com/alkemy/ong/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.alkemy.ong.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Override
   protected void configure(HttpSecurity security) throws Exception {
