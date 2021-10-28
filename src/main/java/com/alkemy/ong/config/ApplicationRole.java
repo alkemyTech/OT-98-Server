8ff304a37816c69cfbdadf4711352f3eda27397c
Franco Zanardi <francozanardi.97@gmail.com>
2021-10-28 15:58:12 -0300

    Applying the correct style to ApplicationRole

--- src/main/java/com/alkemy/ong/config/ApplicationRole.java
+++ src/main/java/com/alkemy/ong/config/ApplicationRole.java
@@ -1,16 +1,16 @@
 package com.alkemy.ong.config;
 
 public enum ApplicationRole {
-    USER("USER"),
-    ADMIN("ADMIN");
+  USER("USER"),
+  ADMIN("ADMIN");
 
-    private final String roleName;
+  private final String roleName;
 
-    ApplicationRole(String roleName) {
-        this.roleName = roleName;
-    }
+  ApplicationRole(String roleName) {
+    this.roleName = roleName;
+  }
 
-    public String getRoleName() {
-        return roleName;
-    }
+  public String getRoleName() {
+    return roleName;
+  }
 }
