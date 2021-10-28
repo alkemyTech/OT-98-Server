75d11ee09332e9e4727afd767b78a7a3bdb82a9b
LucioSandoval <lucio.scaceres@gmail.com>
2021-10-27 22:09:48 -0300

    slide and repository modified

--- src/main/java/com/alkemy/ong/model/entity/Slide.java
+++ src/main/java/com/alkemy/ong/model/entity/Slide.java
@@ -1,7 +1,14 @@
 package com.alkemy.ong.model.entity;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "SLIDES")
