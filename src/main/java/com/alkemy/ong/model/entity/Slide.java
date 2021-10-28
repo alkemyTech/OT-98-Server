3173d54411a55fc80244052c469db7ee17e4efaf
LucioSandoval <lucio.scaceres@gmail.com>
2021-10-27 22:30:47 -0300

    slide and repository modified

--- src/main/java/com/alkemy/ong/model/entity/Slide.java
+++ src/main/java/com/alkemy/ong/model/entity/Slide.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.model.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,13 +10,23 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "SLIDES")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Slide {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SLIDES_ID", nullable = false)
+    @Setter(AccessLevel.NONE)
     private long id;
 
     @Column(name = "IMAGE_URL")
@@ -24,10 +35,10 @@ public class Slide {
     @Column(name = "TEXT")
     private String text;
 
-    @Column(name = "ORDER")
+    @Column(name = "SLIDE_ORDER")
     private int order;
 
     @JoinColumn(name = "ORGANIZATION_ID")
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.PERSIST)
     private Organization organizationId;
 }
