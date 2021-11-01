c09b5e517bf1a26fd12e962782392399455be5da
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-01 04:58:41 -0300

    No arguments constructor added

--- src/main/java/com/alkemy/ong/model/response/UserRegisterResponse.java
+++ src/main/java/com/alkemy/ong/model/response/UserRegisterResponse.java
@@ -3,12 +3,14 @@ package com.alkemy.ong.model.response;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @JsonRootName("User token")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserRegisterResponse {
 
   private String jwt;
