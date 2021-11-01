d15dc93ce15fe62884ff909b9e6d07688b86dca5
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-01 14:52:36 -0300

    OT98-49: convert User and jwt to RegisterResponse is static now

--- src/main/java/com/alkemy/ong/common/converter/ConvertUtils.java
+++ src/main/java/com/alkemy/ong/common/converter/ConvertUtils.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component("convertUtils")
 public class ConvertUtils {
 
-  public UserRegisterResponse toResponse(User user, String jwt) {
+  public static UserRegisterResponse toResponse(User user, String jwt) {
     UserRegisterResponse userRegisterResponse = new UserRegisterResponse();
     userRegisterResponse.setId(user.getId());
     userRegisterResponse.setFirstsName(user.getFirstName());
