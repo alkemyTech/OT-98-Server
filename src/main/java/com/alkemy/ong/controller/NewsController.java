d45bda9f4cae03ece96da103921df871a261906a
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-03 01:47:19 -0300

    OT98-46: News controller added

---
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.alkemy.ong.exception.EntityAlreadyExistException;
+import com.alkemy.ong.model.request.CreateNewsRequest;
+import com.alkemy.ong.service.abstraction.ICreateNewsService;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+
+  @Autowired
+  private ICreateNewsService createNewsService;
+
+  @PostMapping
+  public ResponseEntity<?> create(@RequestBody CreateNewsRequest createNewsRequest)
+      throws EntityAlreadyExistException {
+    return ResponseEntity.ok(createNewsService.create(createNewsRequest));
+  }
+}
