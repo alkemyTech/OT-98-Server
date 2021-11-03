f0d14555612abb917afbb9f7d18d9557aecee3c6
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-02 23:35:21 -0300

    OT98-46: Request and response added

---
+++ src/main/java/com/alkemy/ong/model/request/CreateNewsRequest.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.model.request;
+
+import javax.validation.constraints.NotBlank;
+import com.alkemy.ong.common.validation.ValidationMessages;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateNewsRequest {
+
+  @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
+  private String name;
+
+  @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
+  private String content;
+
+  @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
+  private String image;
+
+  @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
+  private String categoryName;
+}
---
+++ src/main/java/com/alkemy/ong/model/response/CreateNewsResponse.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.model.response;
+
+import com.alkemy.ong.model.entity.Category;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonRootName("news")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateNewsResponse {
+
+  private String name;
+  private String content;
+  private String image;
+  private Category category;
+}
