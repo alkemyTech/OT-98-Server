e3cdd2b68134a19fd577aaf9eeff7b2abf095cbc
Magali Kain <mkain@mulesoft.com>
2021-11-03 20:08:28 -0300

    OT98-39: minor changes from PR review

--- src/main/java/com/alkemy/ong/common/s3/UploadS3ObjectHelper.java
+++ src/main/java/com/alkemy/ong/common/s3/UploadS3ObjectHelper.java
@@ -1,5 +1,6 @@
-package com.alkemy.ong.common;
+package com.alkemy.ong.common.s3;
 
+import com.alkemy.ong.common.FileUtils;
 import com.alkemy.ong.config.AmazonConfig;
 import com.alkemy.ong.exception.ExternalServiceException;
 import com.amazonaws.AmazonClientException;
@@ -10,32 +11,34 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.InputStream;
 import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class UploadImage {
+@Service
+public class UploadS3ObjectHelper {
 
   @Autowired
   private AmazonConfig amazonConfig;
+
   @Autowired
-  private AmazonS3 amazonS3 = amazonConfig.initialize();
+  private AmazonS3 amazonS3;
 
   public String upload(InputStream inputStream, String fileName, ContentType contentType)
       throws ExternalServiceException {
     String bucketName = amazonConfig.getBucketName();
-    String validatedFileName = FileUtils.getFileNameOrDefault(fileName);
-    String fileUrl = "";
+    String fileNameOrDefault = FileUtils.getFileNameOrDefault(fileName);
     try {
       ObjectMetadata metadata = new ObjectMetadata();
       metadata.setContentType(contentType.toString());
-      uploadImageTos3Bucket(validatedFileName, inputStream, bucketName, metadata);
-      fileUrl = amazonS3.getUrl(bucketName, validatedFileName).toString();
+      uploadImageTos3Bucket(fileNameOrDefault, inputStream, bucketName, metadata);
+      return amazonS3.getUrl(bucketName, fileNameOrDefault).toString();
     } catch (AmazonClientException e) {
-      throw new ExternalServiceException("The file could not be uploaded");
+      throw new ExternalServiceException("The file could not be uploaded.");
     }
-    return fileUrl;
   }
 
-  private void uploadImageTos3Bucket(String validatedFileName, InputStream inputStream, String bucketName, ObjectMetadata metadata) {
-    amazonS3.putObject(new PutObjectRequest(bucketName, validatedFileName, inputStream, metadata)
+  private void uploadImageTos3Bucket(String fileName, InputStream inputStream,
+      String bucketName, ObjectMetadata metadata) {
+    amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, metadata)
         .withCannedAcl(CannedAccessControlList.PublicRead));
   }
 }
