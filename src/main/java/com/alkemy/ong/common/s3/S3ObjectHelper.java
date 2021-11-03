4d13be7c1bbe3e5be0612b9c794863007dfd5813
Magali Kain <mkain@mulesoft.com>
2021-11-03 20:10:20 -0300

    OT98-39: rename class

--- src/main/java/com/alkemy/ong/common/s3/S3ObjectHelper.java
+++ src/main/java/com/alkemy/ong/common/s3/S3ObjectHelper.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UploadS3ObjectHelper {
+public class S3ObjectHelper {
 
   @Autowired
   private AmazonConfig amazonConfig;
