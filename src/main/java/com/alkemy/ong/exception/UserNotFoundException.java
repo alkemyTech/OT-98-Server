0a892857e1ddda67bfadd6c99c7dc047c86b62e9
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-04 14:48:13 -0300

    UserNotFoundException class created

---
+++ src/main/java/com/alkemy/ong/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.exception;
+
+public class UserNotFoundException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
