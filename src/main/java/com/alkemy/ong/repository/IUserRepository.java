b144c5ff8ff523e35515ec3af64744db9a11d771
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-04 14:55:15 -0300

    OT98-32: deleteUserById & getUserById methods added

--- src/main/java/com/alkemy/ong/repository/IUserRepository.java
+++ src/main/java/com/alkemy/ong/repository/IUserRepository.java
@@ -11,5 +11,6 @@ public interface IUserRepository extends JpaRepository<User, Long> {
 
   User findByEmail(String email);
   List<User> findBySoftDeletedFalse();
-
+  void deleteUserById(Long id);
+  User getUserById(Long id);
 }
