4c25175603a986715ffcf1d457a9a4bd6f2fb4ef
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-04 00:18:31 -0300

    OT98-46: Added consumes and produces

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.controller;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,8 @@ public class NewsController {
   @Autowired
   private ConvertUtils convertUtils;
 
-  @PostMapping
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CreateNewsResponse> create(
       @RequestBody(required = true) @Valid CreateNewsRequest createNewsRequest)
       throws EntityNotExistException {
