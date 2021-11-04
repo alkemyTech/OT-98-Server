06b8c099c7d243e223598ddebc3c72258ca3625d
Joaquin Aman <Joaquin Aman>
2021-11-04 20:02:24 -0300

    OT98-74: adding Comment entity

---
+++ src/main/java/com/alkemy/ong/model/entity/Comment.java
@@ -0,0 +1,44 @@
+package com.alkemy.ong.model.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "COMMENTS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "COMMENTS_ID", nullable = false)
+  @Setter(AccessLevel.NONE)
+  private long id;
+
+  @Column(name = "BODY", nullable = false)
+  private String body;
+
+  @JoinColumn(name = "USERS_ID")
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+  private User userId;
+
+  @JoinColumn(name = "NEWS_ID")
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+  private News newsId;
+
+
+}
