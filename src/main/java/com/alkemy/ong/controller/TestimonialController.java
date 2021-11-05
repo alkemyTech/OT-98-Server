54feb3d5ab28f48ce0fc784509fb037221b4b477
LucioSandoval <lucio.scaceres@gmail.com>
2021-11-04 22:32:56 -0300

    Create endpoint Post Testimonial formato

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,18 +1,19 @@
-package com.alkemy.ong.controller;
-import com.alkemy.ong.common.converter.ConvertUtils;
-import com.alkemy.ong.model.entity.Testimonial;
-import com.alkemy.ong.model.request.CreateTestimonialRequest;
-import com.alkemy.ong.model.response.CreateTestimonialResponse;
-import com.alkemy.ong.service.abstraction.ICreateTestimonialService;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+  package com.alkemy.ong.controller;
+
+  import com.alkemy.ong.common.converter.ConvertUtils;
+  import com.alkemy.ong.model.entity.Testimonial;
+  import com.alkemy.ong.model.request.CreateTestimonialRequest;
+  import com.alkemy.ong.model.response.CreateTestimonialResponse;
+  import com.alkemy.ong.service.abstraction.ICreateTestimonialService;
+  import javax.validation.Valid;
+  import org.springframework.beans.factory.annotation.Autowired;
+  import org.springframework.http.HttpStatus;
+  import org.springframework.http.MediaType;
+  import org.springframework.http.ResponseEntity;
+  import org.springframework.web.bind.annotation.PostMapping;
+  import org.springframework.web.bind.annotation.RequestBody;
+  import org.springframework.web.bind.annotation.RequestMapping;
+  import org.springframework.web.bind.annotation.RestController;
 
   @RestController
   @RequestMapping("/testimonials")
