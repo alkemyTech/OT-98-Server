163fc710e4aeff55cdba767a1be70dad11089d28
LucioSandoval <lucio.scaceres@gmail.com>
2021-11-05 21:32:38 -0300

    CreateTestimonialRequest

--- src/main/java/com/alkemy/ong/model/request/CreateTestimonialRequest.java
+++ src/main/java/com/alkemy/ong/model/request/CreateTestimonialRequest.java
@@ -1,21 +1,22 @@
-package com.alkemy.ong.model.request;
-import com.alkemy.ong.common.validation.ValidationMessages;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+  package com.alkemy.ong.model.request;
+  import com.alkemy.ong.common.validation.ValidationMessages;
+  import javax.validation.constraints.NotBlank;
+  import javax.validation.constraints.Size;
+  import lombok.Getter;
+  import lombok.Setter;
 
-@Getter
-@Setter
-public class CreateTestimonialRequest {
+  @Getter
+  @Setter
+  public class CreateTestimonialRequest {
   @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
   @Size(max = 250, message = ValidationMessages.REQUEST_PARAM_MAX_ERROR_MESSAGE)
   private String name;
 
   private String image;
+
   @NotBlank(message = ValidationMessages.REQUEST_PARAM_EMPTY_ERROR_MESSAGE)
   @Size(max = 250, message = ValidationMessages.REQUEST_PARAM_MAX_ERROR_MESSAGE)
   private String content;
 
 
-}
+  }
