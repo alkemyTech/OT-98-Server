1493cb82461d51a5a77acba7c1ce2a0145b45f7d
Magali Kain <mkain@mulesoft.com>
2021-11-07 20:07:04 -0300

    Minor changes from Sprint demo to have better contract response

--- src/main/java/com/alkemy/ong/model/response/CreateContactResponse.java
+++ src/main/java/com/alkemy/ong/model/response/CreateContactResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.model.response;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonRootName("contact")
 public class CreateContactResponse {
 
   private long id;
