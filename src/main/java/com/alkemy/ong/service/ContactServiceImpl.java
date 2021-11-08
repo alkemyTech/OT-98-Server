eedeaf6037834bcc3fc76c0fa0e6a6b4ac027d77
Oscar Ruina <oscarruina@hotmail.com>
2021-11-08 10:59:09 -0300

    OT98-61 add get contacts implementation

--- src/main/java/com/alkemy/ong/service/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/ContactServiceImpl.java
@@ -1,17 +1,25 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.common.converter.ConvertUtils;
 import com.alkemy.ong.model.entity.Contact;
 import com.alkemy.ong.model.request.CreateContactRequest;
+import com.alkemy.ong.model.response.CreateContactResponse;
 import com.alkemy.ong.repository.IContactRepository;
 import com.alkemy.ong.service.abstraction.ICreateContactService;
+import com.alkemy.ong.service.abstraction.IListContactsService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ContactServiceImpl implements ICreateContactService {
+public class ContactServiceImpl implements ICreateContactService, IListContactsService {
 
   @Autowired
   IContactRepository contactRepository;
+
+  @Autowired
+  ConvertUtils convertUtils;
 
   @Override
   public Contact create(CreateContactRequest createContactRequest) {
@@ -23,4 +31,15 @@ public class ContactServiceImpl implements ICreateContactService {
     return contactRepository.save(contact);
   }
 
+  @Override
+  public List<CreateContactResponse> contacts() {
+    List<Contact> contacts = contactRepository.findAll();
+    List<CreateContactResponse> createContactResponses = new ArrayList<>();
+    contacts.forEach(contact -> {
+      createContactResponses.add(
+          convertUtils.toResponse(contact)
+      );
+    });
+    return createContactResponses;
+  }
 }
