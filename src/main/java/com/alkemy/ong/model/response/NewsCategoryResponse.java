6f09235252f255ff10504100dff4b68735eec94a
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-07 21:35:14 -0300

    OT98-45: Added get response, and NewsCategoryResponse for return the news category

---
+++ src/main/java/com/alkemy/ong/model/response/NewsCategoryResponse.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsCategoryResponse {
+
+  private String name;
+  private String description;
+  private String image;
+}
