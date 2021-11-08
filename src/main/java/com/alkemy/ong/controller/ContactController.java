819c5d3766956f9b6fb54996a5d9031ea598901a
Oscar Ruina <oscarruina@hotmail.com>
2021-11-08 11:05:03 -0300

    OT98-61 add get contacts in controller

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -5,10 +5,13 @@ import com.alkemy.ong.model.entity.Contact;
 import com.alkemy.ong.model.request.CreateContactRequest;
 import com.alkemy.ong.model.response.CreateContactResponse;
 import com.alkemy.ong.service.abstraction.ICreateContactService;
+import com.alkemy.ong.service.abstraction.IListContactsService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,9 @@ public class ContactController {
   ICreateContactService createContactService;
 
   @Autowired
+  IListContactsService listContactsService;
+
+  @Autowired
   ConvertUtils convertUtils;
 
   @PostMapping(
@@ -33,4 +39,10 @@ public class ContactController {
     CreateContactResponse createContactResponse = this.convertUtils.toResponse(contact);
     return new ResponseEntity<>(createContactResponse, HttpStatus.CREATED);
   }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<CreateContactResponse>> contacts() {
+    return new ResponseEntity<>(listContactsService.contacts(), HttpStatus.OK);
+  }
+  
 }
