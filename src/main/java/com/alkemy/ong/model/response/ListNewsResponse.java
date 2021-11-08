169c93fc3f2c35a09836eae4d46569db069f944b
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-08 19:14:59 -0300

    OT98-81: ListNewsResponse addede

---
+++ src/main/java/com/alkemy/ong/model/response/ListNewsResponse.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.model.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListNewsResponse {
+
+  private List<NewsDetailsResponse> news;
+}
