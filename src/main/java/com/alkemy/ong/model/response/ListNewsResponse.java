5e964da6529c15da95772bb7da6c11552750a0da
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-09 01:19:19 -0300

    OT98-81: JsonRootName added to ListNewsResponse

--- src/main/java/com/alkemy/ong/model/response/ListNewsResponse.java
+++ src/main/java/com/alkemy/ong/model/response/ListNewsResponse.java
@@ -1,11 +1,13 @@
 package com.alkemy.ong.model.response;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@JsonRootName("news")
 @Getter
 @Setter
 @NoArgsConstructor
