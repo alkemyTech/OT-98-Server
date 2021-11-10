6b882c7aa9056446e1deb1cf573afdb8243b32f8
Joaquin Aman <Joaquin Aman>
2021-11-10 16:00:43 -0300

    OT98-76: adding Comment Controller

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.alkemy.ong.common.converter.ConvertUtils;
+import com.alkemy.ong.exception.NewsNotFoundException;
+import com.alkemy.ong.exception.UserNotFoundException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 import com.alkemy.ong.model.response.CreateCommentResponse;
@@ -26,7 +28,8 @@ public class CommentController {
   @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CreateCommentResponse> create(
-      @Valid @RequestBody CreateCommentRequest createCommentRequest) {
+      @Valid @RequestBody CreateCommentRequest createCommentRequest)
+      throws UserNotFoundException, NewsNotFoundException {
     Comment comment = createCommentService.create(createCommentRequest);
     CreateCommentResponse createCommentResponse = convertUtils.toResponse(comment);
     return new ResponseEntity<>(createCommentResponse, HttpStatus.CREATED);
