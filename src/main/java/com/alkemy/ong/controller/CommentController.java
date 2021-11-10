f06f0ae468157ccb999bb27baeaabf7961466449
Joaquin Aman <Joaquin Aman>
2021-11-09 23:42:14 -0300

    ot98-76: adding CommentController

---
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.alkemy.ong.controller;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.alkemy.ong.common.converter.ConvertUtils;
+import com.alkemy.ong.model.entity.Comment;
+import com.alkemy.ong.model.request.CreateCommentRequest;
+import com.alkemy.ong.model.response.CreateCommentResponse;
+import com.alkemy.ong.service.abstraction.ICreateCommentService;
+
+@RestController
+public class CommentController {
+
+  @Autowired
+  ICreateCommentService createCommentService;
+
+  @Autowired
+  private ConvertUtils convertUtils;
+
+  @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<CreateCommentResponse> create(
+      @Valid @RequestBody CreateCommentRequest createCommentRequest) {
+    Comment comment = createCommentService.create(createCommentRequest);
+    CreateCommentResponse createCommentResponse = convertUtils.toResponse(comment);
+    return new ResponseEntity<>(createCommentResponse, HttpStatus.CREATED);
+  }
+}
