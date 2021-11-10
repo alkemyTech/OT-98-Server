28273bb1ed5cd140be3dc09b7214f265127a8159
Joaquin Aman <Joaquin Aman>
2021-11-10 20:24:04 -0300

    OT98-76: fixing CreateCommentResponse

--- src/main/java/com/alkemy/ong/model/response/CreateCommentResponse.java
+++ src/main/java/com/alkemy/ong/model/response/CreateCommentResponse.java
@@ -1,8 +1,10 @@
 package com.alkemy.ong.model.response;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonRootName("comment")
 @Getter
 @Setter
 public class CreateCommentResponse {
