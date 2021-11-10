71063149a22da95e3a30959abce0d39833544932
Joaquin Aman <Joaquin Aman>
2021-11-10 20:25:28 -0300

    OT98-76: fixing ICreateCommentService

--- src/main/java/com/alkemy/ong/service/abstraction/ICreateCommentService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/ICreateCommentService.java
@@ -1,12 +1,9 @@
 package com.alkemy.ong.service.abstraction;
 
-import com.alkemy.ong.exception.NewsNotFoundException;
-import com.alkemy.ong.exception.UserNotFoundException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 
 public interface ICreateCommentService {
 
-  Comment create(CreateCommentRequest createCommentRequest)
-      throws UserNotFoundException, NewsNotFoundException;
+  Comment create(CreateCommentRequest createCommentRequest, String authorizationHeader);
 }
