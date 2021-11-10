7a9e53621bb07bdf14b25df0c7a60bfa5affa3df
Joaquin Aman <Joaquin Aman>
2021-11-10 16:02:58 -0300

    OT98-76: adding exceptions to ICreateCommentService

--- src/main/java/com/alkemy/ong/service/abstraction/ICreateCommentService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/ICreateCommentService.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.service.abstraction;
 
+import com.alkemy.ong.exception.NewsNotFoundException;
+import com.alkemy.ong.exception.UserNotFoundException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 
 public interface ICreateCommentService {
 
-  Comment create(CreateCommentRequest createCommentRequest);
+  Comment create(CreateCommentRequest createCommentRequest)
+      throws UserNotFoundException, NewsNotFoundException;
 }
