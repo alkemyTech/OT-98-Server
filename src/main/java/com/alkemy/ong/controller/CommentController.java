1f18cd2bce493de853d143939fed9148bec78664
Joaquin Aman <Joaquin Aman>
2021-11-10 20:20:00 -0300

    OT98-76: fixing CommentController

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -7,10 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import com.alkemy.ong.common.converter.ConvertUtils;
-import com.alkemy.ong.exception.NewsNotFoundException;
-import com.alkemy.ong.exception.UserNotFoundException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 import com.alkemy.ong.model.response.CreateCommentResponse;
@@ -28,9 +27,9 @@ public class CommentController {
   @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CreateCommentResponse> create(
-      @Valid @RequestBody CreateCommentRequest createCommentRequest)
-      throws UserNotFoundException, NewsNotFoundException {
-    Comment comment = createCommentService.create(createCommentRequest);
+      @Valid @RequestBody CreateCommentRequest createCommentRequest,
+      @RequestHeader(value = "Authorization") String authorizationHeader) {
+    Comment comment = createCommentService.create(createCommentRequest, authorizationHeader);
     CreateCommentResponse createCommentResponse = convertUtils.toResponse(comment);
     return new ResponseEntity<>(createCommentResponse, HttpStatus.CREATED);
   }
