d10d858bf8aa6e74061dbcbaba7ad5ba5279e35c
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-10 17:42:49 -0300

    OT98-47: News details request added

---
+++ src/main/java/com/alkemy/ong/model/request/NewsDetailsRequest.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NewsDetailsRequest {
+
+  private String name;
+  private String content;
+  private String image;
+}
