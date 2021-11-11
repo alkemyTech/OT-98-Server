7295fbdc365d1da700358d222348a7262f27d3ae
alexB0999 <alexbahi.99@gmail.com>
2021-11-10 21:03:03 -0300

    OT98-41: Details of category endpoint implemented.

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -5,12 +5,17 @@ import com.alkemy.ong.exception.EntityAlreadyExistException;
 import com.alkemy.ong.model.entity.Category;
 import com.alkemy.ong.model.request.CreateCategoryRequest;
 import com.alkemy.ong.model.response.CreateCategoryResponse;
+import com.alkemy.ong.model.response.DetailsCategoryResponse;
 import com.alkemy.ong.service.abstraction.ICreateCategoryService;
+import com.alkemy.ong.service.abstraction.IGetCategoryService;
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,9 @@ public class CategoryController {
 
   @Autowired
   private ICreateCategoryService createCategoryService;
+
+  @Autowired
+  private IGetCategoryService getCategoryService;
 
   @Autowired
   private ConvertUtils convertUtils;
@@ -33,6 +41,11 @@ public class CategoryController {
     Category category = createCategoryService.create(createCategoryRequest);
     CreateCategoryResponse createCategoryResponse = convertUtils.toResponse(category);
     return new ResponseEntity<>(createCategoryResponse, HttpStatus.CREATED);
+  }
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<DetailsCategoryResponse> getCategoryDetails(@PathVariable("id") long id) throws EntityNotFoundException {
+    return new ResponseEntity<>(getCategoryService.getBy(id), HttpStatus.OK);
   }
 
 }
--- src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
@@ -3,14 +3,18 @@ package com.alkemy.ong.service;
 import com.alkemy.ong.exception.EntityAlreadyExistException;
 import com.alkemy.ong.model.entity.Category;
 import com.alkemy.ong.model.request.CreateCategoryRequest;
+import com.alkemy.ong.model.response.DetailsCategoryResponse;
 import com.alkemy.ong.repository.ICategoryRepository;
 import com.alkemy.ong.service.abstraction.ICreateCategoryService;
+import com.alkemy.ong.service.abstraction.IGetCategoryService;
+import javax.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoryServiceImpl implements ICreateCategoryService {
+public class CategoryServiceImpl implements ICreateCategoryService, IGetCategoryService {
 
   @Autowired
   private ICategoryRepository categoryRepository;
@@ -29,4 +33,17 @@ public class CategoryServiceImpl implements ICreateCategoryService {
     return category;
   }
 
+  @Override
+  public DetailsCategoryResponse getBy(Long id) throws EntityNotFoundException {
+    Category category = categoryRepository.getById(id);
+    validateCategory(category);
+    ModelMapper modelMapper = new ModelMapper();
+    return modelMapper.map(category, DetailsCategoryResponse.class);
+  }
+
+  private void validateCategory(Category category) {
+    if (category == null) {
+      throw new EntityNotFoundException("The requested resource could not be found.");
+    }
+  }
 }
