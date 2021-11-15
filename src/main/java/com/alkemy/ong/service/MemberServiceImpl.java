e5e34628ee84adcb2df664cad5f55a750a54c396
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-15 11:19:27 -0300

    OT98-70: delete member method implemented

--- src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
@@ -5,13 +5,17 @@ import com.alkemy.ong.model.entity.Member;
 import com.alkemy.ong.model.response.DetailsMemberResponse;
 import com.alkemy.ong.model.response.ListMemberResponse;
 import com.alkemy.ong.repository.IMemberRepository;
+import com.alkemy.ong.service.abstraction.IDeleteMembersService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
 import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberServiceImpl implements IListMembersService {
+public class MemberServiceImpl implements IListMembersService, IDeleteMembersService {
 
   @Autowired
   IMemberRepository memberRepository;
@@ -26,4 +30,12 @@ public class MemberServiceImpl implements IListMembersService {
     return new ListMemberResponse(detailsMemberResponses);
   }
 
+  @Override
+  @Transactional
+  public void deleteBy(long id) throws EntityNotFoundException {
+    Member member = memberRepository.getById(id);
+    if (member == null) throw new EntityNotFoundException("The requested resource could not be found.");
+    member.setSoftDelete(true);
+    memberRepository.save(member);
+  }
 }
