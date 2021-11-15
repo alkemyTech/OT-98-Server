6e2b6e30d5459faea91db58106ab7a52d510ab93
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-15 11:18:25 -0300

    OT98-70: delete member method added

--- src/main/java/com/alkemy/ong/repository/IMemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/IMemberRepository.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Member;
 import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ public interface IMemberRepository extends JpaRepository<Member, Long> {
 
   List<Member> findBySoftDeleteFalse();
 
+  void deleteBy(long id) throws EntityNotFoundException;
 }
