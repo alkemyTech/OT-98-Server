38a4dd07c51d93df925bbbc59e9dc5127cc5699b
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-15 11:16:00 -0300

    OT98-70: admin authorization to delete members added

--- src/main/java/com/alkemy/ong/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/SecurityConfig.java
@@ -105,6 +105,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .hasAnyRole(ApplicationRole.USER.getName())
         .antMatchers(HttpMethod.GET, "/members")
         .hasAnyRole(ApplicationRole.ADMIN.getName())
+        .antMatchers(HttpMethod.DELETE, "/members/**")
+        .hasAnyRole(ApplicationRole.ADMIN.getName())
         .antMatchers(HttpMethod.PATCH, "/users/**")
         .hasAnyRole(ApplicationRole.ADMIN.getName(), ApplicationRole.USER.getName())
         .anyRequest()
