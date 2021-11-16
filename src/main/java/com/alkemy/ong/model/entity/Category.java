f4869b332934b6b5e894777fed0a358da1f8b537
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-15 23:55:32 -0300

    OT98-93: Hash code and equals was added to News and Category for use eq in tests

--- src/main/java/com/alkemy/ong/model/entity/Category.java
+++ src/main/java/com/alkemy/ong/model/entity/Category.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import org.hibernate.annotations.CreationTimestamp;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "CATEGORIES")
 public class Category {
