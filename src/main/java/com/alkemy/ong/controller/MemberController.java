180a5869ad093b1f6989fb0dc947ea1dcf411ece
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2021-11-16 18:22:13 -0300

    OT98-69: create members endpoint (#54)

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,6 +1,17 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.common.converter.ConvertUtils;
+import com.alkemy.ong.exception.EntityAlreadyExistException;
+import com.alkemy.ong.model.entity.Category;
+import com.alkemy.ong.model.entity.Member;
+import com.alkemy.ong.model.request.CreateCategoryRequest;
+import com.alkemy.ong.model.request.DetailsMemberRequest;
+import com.alkemy.ong.model.response.CreateCategoryResponse;
+import com.alkemy.ong.model.response.DetailsMemberResponse;
 import com.alkemy.ong.model.response.ListMemberResponse;
+import com.alkemy.ong.service.abstraction.ICreateMemberService;
+import com.alkemy.ong.service.abstraction.IListMembersService;
+import javax.validation.Valid;
 import com.alkemy.ong.service.abstraction.IDeleteMembersService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
 import javax.persistence.EntityNotFoundException;
@@ -10,6 +21,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +33,36 @@ public class MemberController {
   IListMembersService listMembersService;
 
   @Autowired
+  ICreateMemberService createMemberService;
+
+  @Autowired
+  private ConvertUtils convertUtils;
+
+  @Autowired
   IDeleteMembersService deleteMembersService;
+
 
   @GetMapping(value = "/members", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ListMemberResponse> list() {
     return new ResponseEntity<>(listMembersService.list(), HttpStatus.OK);
   }
 
+
+  @PostMapping(value = "/members", 
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<DetailsMemberResponse> create(
+      @Valid @RequestBody DetailsMemberRequest detailsMemberRequest) {
+    Member member = createMemberService.create(detailsMemberRequest);
+    DetailsMemberResponse detailsMemberResponse = convertUtils.memberToResponse(member);
+    return new ResponseEntity<>(detailsMemberResponse, HttpStatus.CREATED);
+  }
+  
   @DeleteMapping(value = "/members/{id}")
   public ResponseEntity<?> deleteBy(@PathVariable long id) throws EntityNotFoundException {
     deleteMembersService.deleteBy(id);
     return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
+
   }
 
 }
--- src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
@@ -2,9 +2,16 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.common.converter.ConvertUtils;
 import com.alkemy.ong.model.entity.Member;
+import com.alkemy.ong.model.entity.News;
+import com.alkemy.ong.model.request.CreateCommentRequest;
+import com.alkemy.ong.model.request.DetailsMemberRequest;
 import com.alkemy.ong.model.response.DetailsMemberResponse;
 import com.alkemy.ong.model.response.ListMemberResponse;
 import com.alkemy.ong.repository.IMemberRepository;
+import com.alkemy.ong.service.abstraction.ICreateMemberService;
+import com.alkemy.ong.service.abstraction.IListMembersService;
+import java.util.List;
+import java.util.Optional;
 import com.alkemy.ong.service.abstraction.IDeleteMembersService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
 import java.util.List;
@@ -13,7 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberServiceImpl implements IListMembersService, IDeleteMembersService {
+public class MemberServiceImpl
+    implements IListMembersService, ICreateMemberService, IDeleteMembersService {
+
 
   @Autowired
   IMemberRepository memberRepository;
@@ -29,6 +38,19 @@ public class MemberServiceImpl implements IListMembersService, IDeleteMembersSer
   }
 
   @Override
+  public Member create(DetailsMemberRequest detailsMemberRequest) {
+    Member member = new Member();
+    member.setName(detailsMemberRequest.getName());
+    member.setImage(detailsMemberRequest.getImage());
+    member.setDescription(detailsMemberRequest.getDescription());
+    member.setSoftDelete(false);
+    member.setFacebookUrl(detailsMemberRequest.getFacebookUrl());
+    member.setLinkedinUrl(detailsMemberRequest.getLinkedinUrl());
+    member.setInstagramUrl(detailsMemberRequest.getInstagramUrl());
+
+    return memberRepository.save(member);
+  }
+  @Override
   public void deleteBy(long id) throws EntityNotFoundException {
     Member member = memberRepository.getById(id);
     validateMember(member);
@@ -40,6 +62,7 @@ public class MemberServiceImpl implements IListMembersService, IDeleteMembersSer
     if (member == null) {
       throw new EntityNotFoundException("The requested resource could not be found.");
     }
+
   }
 
 }
