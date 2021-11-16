852f4bfd6632f404a9cf1ba662af9f27c3795306
matiasutn2019 <75179524+matiasutn2019@users.noreply.github.com>
2021-11-16 10:59:27 -0300

    OT98-70: delete members endpoint (#55)

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.model.response.ListMemberResponse;
+import com.alkemy.ong.service.abstraction.IDeleteMembersService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +19,18 @@ public class MemberController {
   @Autowired
   IListMembersService listMembersService;
 
+  @Autowired
+  IDeleteMembersService deleteMembersService;
+
   @GetMapping(value = "/members", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ListMemberResponse> list() {
     return new ResponseEntity<>(listMembersService.list(), HttpStatus.OK);
+  }
+
+  @DeleteMapping(value = "/members/{id}")
+  public ResponseEntity<?> deleteBy(@PathVariable long id) throws EntityNotFoundException {
+    deleteMembersService.deleteBy(id);
+    return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
   }
 
 }
