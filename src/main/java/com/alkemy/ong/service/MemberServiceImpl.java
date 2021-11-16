65cf59f2eb22a0ae15a2e4d7463021a7e56ddd53
Joaquin Aman <Joaquin Aman>
2021-11-15 21:56:47 -0300

    OT98-69: adding create method to MemberServiceImpl

--- src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
@@ -2,16 +2,23 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.common.converter.ConvertUtils;
 import com.alkemy.ong.model.entity.Member;
+import com.alkemy.ong.model.entity.News;
+import com.alkemy.ong.model.request.CreateCommentRequest;
+import com.alkemy.ong.model.request.CreateMemberRequest;
 import com.alkemy.ong.model.response.DetailsMemberResponse;
 import com.alkemy.ong.model.response.ListMemberResponse;
 import com.alkemy.ong.repository.IMemberRepository;
+
+import com.alkemy.ong.service.abstraction.ICreateMemberService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
 import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberServiceImpl implements IListMembersService {
+public class MemberServiceImpl implements IListMembersService, ICreateMemberService {
 
   @Autowired
   IMemberRepository memberRepository;
@@ -24,6 +31,15 @@ public class MemberServiceImpl implements IListMembersService {
     List<Member> members = memberRepository.findBySoftDeleteFalse();
     List<DetailsMemberResponse> detailsMemberResponses = convertUtils.toResponseList(members);
     return new ListMemberResponse(detailsMemberResponses);
+  }
+
+  @Override
+  public Member create(CreateMemberRequest createMemberRequest) {
+    Member member = new Member();
+    member.setName(createMemberRequest.getName());
+    member.setImage(createMemberRequest.getImage());
+    memberRepository.save(member);
+    return member;
   }
 
 }
