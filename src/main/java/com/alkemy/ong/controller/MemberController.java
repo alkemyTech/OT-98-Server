fed279bf71c482ac65dc8b903f68abe1aaa04a61
Joaquin Aman <Joaquin Aman>
2021-11-15 21:56:06 -0300

    OT98-69: adding PostMapping to MemberController

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,12 +1,24 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.common.converter.ConvertUtils;
+import com.alkemy.ong.exception.EntityAlreadyExistException;
+import com.alkemy.ong.model.entity.Category;
+import com.alkemy.ong.model.entity.Member;
+import com.alkemy.ong.model.request.CreateCategoryRequest;
+import com.alkemy.ong.model.request.CreateMemberRequest;
+import com.alkemy.ong.model.response.CreateCategoryResponse;
+import com.alkemy.ong.model.response.CreateMemberResponse;
 import com.alkemy.ong.model.response.ListMemberResponse;
+import com.alkemy.ong.service.abstraction.ICreateMemberService;
 import com.alkemy.ong.service.abstraction.IListMembersService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +27,25 @@ public class MemberController {
   @Autowired
   IListMembersService listMembersService;
 
+  @Autowired
+  ICreateMemberService createMemberService;
+
+  @Autowired
+  private ConvertUtils convertUtils;
+
   @GetMapping(value = "/members", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ListMemberResponse> list() {
     return new ResponseEntity<>(listMembersService.list(), HttpStatus.OK);
+  }
+
+  @PostMapping(value = "/members", 
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<CreateMemberResponse> create(
+      @Valid @RequestBody CreateMemberRequest createMemberRequest) {
+    Member member = createMemberService.create(createMemberRequest);
+    CreateMemberResponse createMemberResponse = convertUtils.MemberToResponse(member);
+    return new ResponseEntity<>(createMemberResponse, HttpStatus.CREATED);
   }
 
 }
