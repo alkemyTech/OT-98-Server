79ef10ed7438986582f6dd46a5a368a912aeecde
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-17 16:38:48 -0300

    OT98-55: GetMapping to list added

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,11 +1,15 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.model.response.ListSlidesResponse;
 import com.alkemy.ong.service.abstraction.IDeleteSlideService;
+import com.alkemy.ong.service.abstraction.IListSlidesService;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +21,18 @@ public class SlideController {
   @Autowired
   private IDeleteSlideService deleteSlideService;
 
+  @Autowired
+  private IListSlidesService listSlidesService;
+
   @DeleteMapping("/{id}")
   public ResponseEntity<?> delete(@PathVariable("id") long id) throws EntityNotFoundException {
     deleteSlideService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ListSlidesResponse> list() {
+    ListSlidesResponse listSlidesResponse = listSlidesService.list();
+    return new ResponseEntity<>(listSlidesResponse, HttpStatus.OK);
+  }
 }
