2f7e899aa05cac1147fc8f469dba667a0b7332ec
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-18 09:58:23 -0300

    OT98-57: create method added to slide service

--- src/main/java/com/alkemy/ong/service/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/SlideServiceImpl.java
@@ -1,14 +1,23 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.common.s3.S3ObjectHelper;
+import com.alkemy.ong.exception.ExternalServiceException;
+import com.alkemy.ong.model.entity.Slide;
+import com.alkemy.ong.model.request.CreateSlideRequest;
 import com.alkemy.ong.repository.ISlideRepository;
+import com.alkemy.ong.service.abstraction.ICreateSlideService;
 import com.alkemy.ong.service.abstraction.IDeleteSlideService;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Base64;
 import javax.persistence.EntityNotFoundException;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class SlideServiceImpl implements IDeleteSlideService {
+public class SlideServiceImpl implements IDeleteSlideService, ICreateSlideService {
 
   @Autowired
   private ISlideRepository slideRepository;
@@ -22,4 +31,27 @@ public class SlideServiceImpl implements IDeleteSlideService {
     slideRepository.deleteById(id);
   }
 
+  @Transactional
+  @Override
+  public Slide create(ContentType contentType, String fileName, CreateSlideRequest createSlideRequest)
+      throws EntityNotFoundException, ExternalServiceException, NullPointerException {
+
+    if (createSlideRequest.getImage() == null) throw new NullPointerException();
+
+    byte[] decodedBytes = Base64.getDecoder().decode(createSlideRequest.getImage());
+    InputStream inputStream = new ByteArrayInputStream(decodedBytes);
+
+    S3ObjectHelper s3ObjectHelper = new S3ObjectHelper();
+
+    Slide slide = new Slide();
+
+    String image_Url = s3ObjectHelper.upload(inputStream,fileName, contentType);
+    slide.setImage_Url(image_Url);
+    slide.setText(createSlideRequest.getText());
+    if(createSlideRequest.getOrder() == 0) {
+      slide.setOrder(slideRepository.getMaxOrder() + 1);
+    }
+    slide.setOrder(createSlideRequest.getOrder());
+    return slideRepository.save(slide);
+  }
 }
