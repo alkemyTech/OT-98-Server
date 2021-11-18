1e4dbd8fbba60690fa3b19120655a65dcca58773
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-18 09:57:36 -0300

    OT98-57: get max slide order added to slide repo

--- src/main/java/com/alkemy/ong/repository/ISlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/ISlideRepository.java
@@ -2,9 +2,13 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISlideRepository extends JpaRepository<Slide, Long> {
+
+  @Query(value = "SELECT MAX(SLIDE_ORDER) FROM SLIDES", nativeQuery = true)
+  int getMaxOrder() throws NullPointerException;
 
 }
