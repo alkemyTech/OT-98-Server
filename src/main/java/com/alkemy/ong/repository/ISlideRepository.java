8df81c9f7abeb53673b3d9df06f1716d866765e6
Alexis Bahi <67486947+alexB0999@users.noreply.github.com>
2021-11-19 11:13:55 -0300

    OT98-60: include slides in getOrganizationDetails (#62)

--- src/main/java/com/alkemy/ong/repository/ISlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/ISlideRepository.java
@@ -1,10 +1,17 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Slide;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISlideRepository extends JpaRepository<Slide, Long> {
+
+  @Query(value = "SELECT * FROM slides s WHERE s.organization_id = :organizationId ORDER BY s.slide_order",
+      nativeQuery = true)
+  List<Slide> findByOrganizationIdOrderBySlideOrder(@Param("organizationId") Long id);
 
 }
