9965196b46dcff72d9ca595c8cc7290f858dcce7
Roque Espinoza <roquealbertoespinoza@gmail.com>
2021-11-19 18:20:15 -0300

    OT98-78: Modificar enum role

--- src/main/java/com/alkemy/ong/model/enums/RoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.alkemy.ong.model.enums;
-
-public enum RoleEnum {
-
-  ROLE_USER,ROLE_ADMIN;
-}
--- src/main/java/com/alkemy/ong/service/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/CommentServiceImpl.java
@@ -1,28 +1,23 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.common.JwtUtil;
+import com.alkemy.ong.config.ApplicationRole;
 import com.alkemy.ong.exception.ForbiddenException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.entity.News;
 import com.alkemy.ong.model.entity.Role;
 import com.alkemy.ong.model.entity.User;
-import com.alkemy.ong.model.enums.RoleEnum;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 import com.alkemy.ong.repository.ICommentRepository;
 import com.alkemy.ong.repository.INewsRepository;
 import com.alkemy.ong.repository.IUserRepository;
 import com.alkemy.ong.service.abstraction.ICreateCommentService;
 import com.alkemy.ong.service.abstraction.IDeleteCommentsService;
-import com.jayway.jsonpath.InvalidModificationException;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
-import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpClientErrorException.Forbidden;
 
 @Service
 public class CommentServiceImpl implements ICreateCommentService, IDeleteCommentsService {
@@ -77,7 +72,7 @@ public class CommentServiceImpl implements ICreateCommentService, IDeleteComment
 
     Comment comment = commentOptional.get();
     User user = getUser(authorizationHeader);
-    boolean roleAdmin = haveRole(RoleEnum.ROLE_ADMIN.toString(), user.getRoles());
+    boolean roleAdmin = haveRole(ApplicationRole.ADMIN.getName(), user.getRoles());
 
     if (comment.getUserId().getId() == user.getId() || roleAdmin) {
       commentRepository.delete(comment);
