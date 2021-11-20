13f08fc884c3662e0d13ee2ad380b688521cba45
Oscar Ruina <oscarruina@hotmail.com>
2021-11-20 12:57:00 -0300

    OT98-82 add pagination to members

--- src/main/java/com/alkemy/ong/repository/IMemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/IMemberRepository.java
@@ -1,13 +1,14 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Member;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMemberRepository extends JpaRepository<Member, Long> {
 
-  List<Member> findBySoftDeleteFalse();
+  Page<Member> findBySoftDeleteFalse(Pageable pageable);
 
 }
