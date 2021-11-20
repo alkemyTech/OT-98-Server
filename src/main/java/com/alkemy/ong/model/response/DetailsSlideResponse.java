c913ed874a3bcc5713ce01a73e78ac1f68862956
Kevin Raimo <kevinraimolugo@gmail.com>
2021-11-19 21:40:49 -0300

    OT98-56: DetailsSlideResponse was changed for return slides text and organization

--- src/main/java/com/alkemy/ong/model/response/DetailsSlideResponse.java
+++ src/main/java/com/alkemy/ong/model/response/DetailsSlideResponse.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.model.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +16,11 @@ import lombok.Setter;
 public class DetailsSlideResponse {
 
   private String image;
+
+  @JsonInclude(Include.NON_NULL)
+  private String text;
   private Integer order;
+
+  @JsonInclude(Include.NON_NULL)
+  private SlideOrganizationResponse organization;
 }
