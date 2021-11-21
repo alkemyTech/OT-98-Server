957591ce067c90238f34abb08c2148219a355212
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-21 14:37:41 -0300

    OT98-75: get all method added

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -4,15 +4,20 @@ import com.alkemy.ong.common.converter.ConvertUtils;
 import com.alkemy.ong.exception.UnableToDeleteObjectException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
+import com.alkemy.ong.model.response.CommentsResponse;
 import com.alkemy.ong.model.response.CreateCommentResponse;
 import com.alkemy.ong.service.abstraction.ICreateCommentService;
 import com.alkemy.ong.service.abstraction.IDeleteCommentsService;
+import com.alkemy.ong.service.abstraction.IListCommentsService;
+import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +36,9 @@ public class CommentController {
   @Autowired
   private ConvertUtils convertUtils;
 
+  @Autowired
+  private IListCommentsService listCommentsService;
+
   @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CreateCommentResponse> create(
@@ -47,6 +55,11 @@ public class CommentController {
       throws UnableToDeleteObjectException {
     deleteCommentsService.delete(id, authorizationHeader);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping(value = "/comments", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<CommentsResponse>> list() throws EntityNotFoundException {
+    return new ResponseEntity<>(listCommentsService.list(), HttpStatus.OK);
   }
 
 }
