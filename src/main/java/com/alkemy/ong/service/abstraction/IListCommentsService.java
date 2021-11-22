f4a668158aa34d432967cb9865fe6dad08536645
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-22 11:30:38 -0300

    OT98-75: type changed

--- src/main/java/com/alkemy/ong/service/abstraction/IListCommentsService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IListCommentsService.java
@@ -1,10 +1,8 @@
 package com.alkemy.ong.service.abstraction;
 
-import com.alkemy.ong.model.response.CommentsResponse;
-import java.util.List;
-import javax.persistence.EntityNotFoundException;
+import com.alkemy.ong.model.response.ListCommentsResponse;
 
 public interface IListCommentsService {
-  List<CommentsResponse> list() throws EntityNotFoundException;
+  ListCommentsResponse list();
 
 }
