01bc846ba0770861a796bd62a86756a0367b56ad
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-22 11:25:56 -0300

    OT98-75: list of comments response created

---
+++ src/main/java/com/alkemy/ong/model/response/ListCommentsResponse.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.model.response;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonRootName("Comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListCommentsResponse {
+
+  private List<CommentsResponse> comments;
+
+}
