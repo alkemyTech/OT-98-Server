0f1a11a208a05487e65153f6d90ef3b65ff3e535
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-22 11:28:54 -0300

    OT98-75: jsonrootname deleted

--- src/main/java/com/alkemy/ong/model/response/CommentsResponse.java
+++ src/main/java/com/alkemy/ong/model/response/CommentsResponse.java
@@ -1,12 +1,10 @@
 package com.alkemy.ong.model.response;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@JsonRootName("comments")
 @Getter
 @Setter
 @NoArgsConstructor
