3042a92b2c0d36991d5385be778f6c56f152c57b
matiasutn2019 <75179524+matiasutn2019@users.noreply.github.com>
2021-11-22 13:09:52 -0300

    OT98-75: list comments endpoint (#74)

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -5,14 +5,17 @@ import com.alkemy.ong.exception.UnableToDeleteObjectException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
 import com.alkemy.ong.model.response.CreateCommentResponse;
+import com.alkemy.ong.model.response.ListCommentsResponse;
 import com.alkemy.ong.service.abstraction.ICreateCommentService;
 import com.alkemy.ong.service.abstraction.IDeleteCommentsService;
+import com.alkemy.ong.service.abstraction.IListCommentsService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +34,9 @@ public class CommentController {
   @Autowired
   private ConvertUtils convertUtils;
 
+  @Autowired
+  private IListCommentsService listCommentsService;
+
   @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CreateCommentResponse> create(
@@ -47,6 +53,11 @@ public class CommentController {
       throws UnableToDeleteObjectException {
     deleteCommentsService.delete(id, authorizationHeader);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping(value = "/comments", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ListCommentsResponse> list() {
+    return new ResponseEntity<>(listCommentsService.list(), HttpStatus.OK);
   }
 
 }
