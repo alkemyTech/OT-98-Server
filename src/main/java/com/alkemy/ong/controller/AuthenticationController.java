239da7868cb4233dd29f9067ca8cc33e63403c7f
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2021-11-23 18:17:49 -0300

    OT98-89: add documentation to authentication endpoints (#78)

--- src/main/java/com/alkemy/ong/controller/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/controller/AuthenticationController.java
@@ -1,9 +1,14 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.common.messages.DocumentationMessages;
 import com.alkemy.ong.exception.InvalidCredentialsException;
 import com.alkemy.ong.model.request.UserAuthenticationRequest;
 import com.alkemy.ong.model.response.UserDetailsResponse;
 import com.alkemy.ong.service.abstraction.IAuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -16,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = DocumentationMessages.AUTHENTICATION_CONTROLLER,
+  description = DocumentationMessages.AUTHENTICATION_CONTROLLER_DESCRIPTION)
 public class AuthenticationController {
 
   @Autowired
@@ -24,7 +31,21 @@ public class AuthenticationController {
   @PostMapping(value = "/login",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<?> login(@RequestBody UserAuthenticationRequest userRequest)
+  @Operation(summary = DocumentationMessages.AUTHENTICATION_CONTROLLER_LOGIN)
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200",
+          description = DocumentationMessages.AUTHENTICATION_CONTROLLER_RESPONSE_200_DESCRIPTION),
+      @ApiResponse(responseCode = "400",
+          description = DocumentationMessages.AUTHENTICATION_CONTROLLER_RESPONSE_400_DESCRIPTION),
+      @ApiResponse(responseCode = "401",
+          description = DocumentationMessages.AUTHENTICATION_CONTROLLER_RESPONSE_401_DESCRIPTION),
+      @ApiResponse(responseCode = "404",
+      description = DocumentationMessages.AUTHENTICATION_CONTROLLER_RESPONSE_404_DESCRIPTION),
+      @ApiResponse(responseCode = "500",
+      description = DocumentationMessages.AUTHENTICATION_CONTROLLER_RESPONSE_500_DESCRIPTION)
+  })
+  public ResponseEntity<UserDetailsResponse> login(
+      @RequestBody UserAuthenticationRequest userRequest)
       throws EntityNotFoundException, AuthenticationException, InvalidCredentialsException {
     UserDetailsResponse user = authenticationService.login(userRequest);
     return ResponseEntity.ok(user);
