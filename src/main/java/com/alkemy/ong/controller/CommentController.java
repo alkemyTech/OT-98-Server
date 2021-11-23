6861a85d1093fe388b157438e2a44bbf1b6bab4a
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-23 11:24:56 -0300

    OT98-77:  update comment method added

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -4,11 +4,14 @@ import com.alkemy.ong.common.converter.ConvertUtils;
 import com.alkemy.ong.exception.UnableToDeleteObjectException;
 import com.alkemy.ong.model.entity.Comment;
 import com.alkemy.ong.model.request.CreateCommentRequest;
+import com.alkemy.ong.model.request.UpdateCommentRequest;
 import com.alkemy.ong.model.response.CreateCommentResponse;
+import com.alkemy.ong.model.response.DetailsCommentResponse;
 import com.alkemy.ong.model.response.ListCommentsResponse;
 import com.alkemy.ong.service.abstraction.ICreateCommentService;
 import com.alkemy.ong.service.abstraction.IDeleteCommentsService;
 import com.alkemy.ong.service.abstraction.IListCommentsService;
+import com.alkemy.ong.service.abstraction.IUpdateCommentService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +40,9 @@ public class CommentController {
 
   @Autowired
   private IListCommentsService listCommentsService;
+
+  @Autowired
+  private IUpdateCommentService updateCommentService;
 
   @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
@@ -60,4 +67,16 @@ public class CommentController {
     return new ResponseEntity<>(listCommentsService.list(), HttpStatus.OK);
   }
 
+  @PutMapping(
+      value = "/comments/{id}",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<DetailsCommentResponse> update(
+      @PathVariable("id") long id,
+      @RequestHeader(value = "Authorization") String authorizationHeader,
+      @Valid @RequestBody(required = true) UpdateCommentRequest updateCommentRequest) {
+    return new ResponseEntity<>(
+        updateCommentService.update(id, authorizationHeader, updateCommentRequest),
+        HttpStatus.OK);
+  }
 }
