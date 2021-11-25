d949d3281fcba74be45e2b990051202ae24bbef0
Joaquin Aman <Joaquin Aman>
2021-11-25 14:39:14 -0300

    OT98-79: adding listing method to ICommentRepository

--- src/main/java/com/alkemy/ong/repository/ICommentRepository.java
+++ src/main/java/com/alkemy/ong/repository/ICommentRepository.java
@@ -1,7 +1,9 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Comment;
+import com.alkemy.ong.model.entity.News;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
 
+  @Override
   @Query("from Comment c order by c.timestamp")
   List<Comment> findAll();
 
+  List<Comment> findByNewsId(Optional<News> news);
 }
