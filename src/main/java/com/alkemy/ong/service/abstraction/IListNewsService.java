e83a5e7dd5ca21605fff376d409c02748ceae4a8
Joaquin Aman <Joaquin Aman>
2021-11-28 17:16:31 -0300

    OT98-79: removing unused imports

--- src/main/java/com/alkemy/ong/service/abstraction/IListNewsService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IListNewsService.java
@@ -1,9 +1,6 @@
 package com.alkemy.ong.service.abstraction;
 
 import com.alkemy.ong.model.entity.News;
-import com.alkemy.ong.model.response.ListCommentsResponse;
-import com.alkemy.ong.model.response.NewsDetailsCommentsResponse;
-import javax.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 
 public interface IListNewsService {
